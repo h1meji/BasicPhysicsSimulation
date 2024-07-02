88d536c397b85427059c628f6f09d5bbd14485de
himeji. <88136704+h1meji@users.noreply.github.com>
2024-07-02 18:50:31 +0200

    ✨ Limit window framerate

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
 int main()
 {
     sf::Window window(sf::VideoMode(800, 600), "Basic Physics Simulation");
 
+    window.setFramerateLimit(60);
+
     while (window.isOpen())
     {
-    
         sf::Event event;
 
         while (window.pollEvent(event))
