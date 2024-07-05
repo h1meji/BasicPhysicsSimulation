bebc69d51bdb5b1180108f1622ccc4a45539a5f6
himeji. <88136704+h1meji@users.noreply.github.com>
2024-07-05 17:46:15 +0200

    💄 Created main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 
+#include "Simulation/Simulation.h"
+
+#include <iostream>
+
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "Basic Physics Simulation");
+    // random seed
+    srand(time(NULL));
 
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Basic Physics Simulation");
     window.setFramerateLimit(60);
 
+    Simulation simulation(window.getSize(), 10, true, true);
+
+    const float timeStep = 1.0f / 60.0f;
     sf::Clock clock;
+    float accumulator = 0.0f;
+    float currentTime = clock.getElapsedTime().asSeconds();
 
     while (window.isOpen())
     {
-        sf::Time elapsed = clock.restart();
         sf::Event event;
 
         while (window.pollEvent(event))
@@ -19,6 +29,27 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        float newTime = clock.getElapsedTime().asSeconds();
+        float frameTime = newTime - currentTime;
+        currentTime = newTime;
+        accumulator += frameTime;
+
+        while (accumulator >= timeStep) {
+            simulation.update(timeStep);
+            accumulator -= timeStep;
+        }
+
+        window.clear();
+
+        // Draw particles
+        for (const auto& particle : simulation.getParticles()) {
+            window.draw(particle.getDrawable());
+        }
+
+        window.display();
+
+        std::cout << accumulator << "\n" << frameTime << "\n" << newTime << "\n" << std::endl;
     }
 
     return 0;
